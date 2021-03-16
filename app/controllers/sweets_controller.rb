4b5032643cfbadc2b84fa1b017fbaa424685bc46
chuntarou <roth_peyo@icloud.com>
2021-03-16 12:47:04 +0900

    Active Storageの導入

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,4 +1,20 @@
 class SweetsController < ApplicationController
   def index
+    @sweets = Sweet.all
+  end
+
+  def new
+    @sweets = Sweet.new
+  end
+
+  def create
+    @sweet = Sweet.new(sweet_params)
+  end
+
+
+private
+
+  def sweet_params
+    params.require(:sweet)permit(:name, :price, :shop_info, :text, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/sweet.rb
+++ app/models/sweet.rb
@@ -1,2 +1,3 @@
 class Sweet < ApplicationRecord
+  has_one_attached :image
 end
