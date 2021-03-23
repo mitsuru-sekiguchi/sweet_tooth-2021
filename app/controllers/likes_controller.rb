ba8229b37b03605caca00ff6f59187457bdbff52
chuntarou <roth_peyo@icloud.com>
2021-03-23 16:06:46 +0900

    いいねを解除する実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,9 @@ class LikesController < ApplicationController
     Like.create(user_id: current_user.id, sweet_id: params[:id])
     redirect_to sweets_path
   end
+
+  def destroy
+    Like.find_by(user_id: current_user.id, sweet_id: params[:id]).destroy
+    redirect_to sweets_path
+  end
 end
