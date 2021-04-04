f3ea779639b55290c3350c2376e773875bc9436c
chuntarou <roth_peyo@icloud.com>
2021-04-04 12:43:41 +0900

    アセットパイプラインの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.assets.digest = false
+  config.assets.digest = true
 end
