f0a335295bec612226e2d7db094d740a8a54f38c
chuntarou <roth_peyo@icloud.com>
2021-04-09 19:07:35 +0900

    簡単ログイン・ゲストログイン機能の実装

--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -46,6 +46,15 @@ class SweetsController < ApplicationController
     redirect_to root_path if sweet.destroy
   end
 
+  def guest_sign_in
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+  end
+
   private
 
   def sweet_params
